bfda591a1d525d6f787a6357ed16edba004a0b89
kaivanwadia <kaivanwadia@gmail.com>
2015-03-31 22:04:45 -0500

    Two test cases. Simple one basic block function with return and no return

---
+++ Output/SimpleBBReturn.c
@@ -0,0 +1,8 @@
+int sum(int x)
+{
+	// x = x + 1;
+	int a = x + 2;
+	int b = a - 3;
+	int c = a + b;
+	return b;
+}
---
+++ Output/SimpleBBVoid.c
@@ -0,0 +1,8 @@
+void sum(int x)
+{
+	x = x + 1;
+	int a = x + 2;
+	int b = a - 3;
+	int c = a + b;
+	return;
+}
