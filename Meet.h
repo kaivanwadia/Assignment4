c2dbd6f6fb2e27ccec7e1427fd1110fee54f725f
Mark Gray <markmgray2010@gmail.com>
2015-04-02 19:50:28 -0500

    updated the meet function

--- Meet.h
+++ Meet.h
@@ -31,6 +31,7 @@ class DCEMeet: public Meet<llvm::StringRef, StringRefHash, StringRefEqual>
 {
 private:
 public:
+	using StringSet = std::unordered_set<StringRef, StringRefHash, StringRefEqual>;
 	DCEMeet() : Meet<llvm::StringRef, StringRefHash, StringRefEqual>() {}
 	bool doMeet(const llvm::BasicBlock* bb, DFAMap& inMap, DFAMap& outMap)
 	{
@@ -44,44 +45,50 @@ public:
 		}
 
 		auto& bbVariables = itr->second;
-		for(auto bbItr = succ_begin(bb); bbItr != succ_end(bb); bbItr++)
+		StringSet intersectedSet;
+		for(auto bbOuter = succ_begin(bb); bbOuter != succ_end(bb); bbOuter++)
 		{
-			auto succVariables = inMap[*bbItr];
-			for(auto const variable : succVariables)
+			auto outerVariables = inMap[*bbOuter];
+			for(auto const variable : outerVariables)
 			{
-				updated |= bbVariables.insert(variable).second;
+				bool present = true;
+				for(auto bbItr = succ_begin(bb); bbItr != succ_end(bb); bbItr++)
+				{
+					if(bbOuter == bbIter) // Same basic block just continue
+						continue;
+
+					auto checkPtr = inMap[*bbItr].find(variable);
+					if(checkPtr == inMap[*bbItr].end())
+					{
+						present = false;
+					}
+				}
+				if(present)
+				{
+					intersectedSet.insert(variable);
+				}
 			}
 		}
-		return updated;
-	}
-};
 
-class DCEMeetPair: public Meet<std::pair<StringRef, STATUS>, StringRefHashPair, StringRefEqualPair>
-{
-private:
-public:
-	using PairSet = std::pair<StringRef, STATUS>;
-	DCEMeetPair() : Meet<PairSet, StringRefHashPair, StringRefEqualPair>() {}
-	bool doMeet(const llvm::BasicBlock* bb, DFAMap& inMap, DFAMap& outMap)
-	{
-		bool updated = false;
-		printf("In doMeet of DCEMeetPair\n");
-		auto itr = outMap.find(bb);
-		if(itr == outMap.end())
+		if(intersectedSet.size() != bbVariables.size())
 		{
 			updated = true;
-			itr = outMap.insert(std::make_pair(bb, TypeSet())).first;
 		}
-
-		auto& bbVariables = itr->second;
-		for(auto bbItr = succ_begin(bb); bbItr != succ_end(bb); bbItr++)
+		else
 		{
-			auto succVariables = inMap[*bbItr];
-			for(auto const variable : succVariables)
+			updated = false;
+			for(auto variable : bbVariables)
 			{
-				updated |= bbVariables.insert(variable).second;
+				auto checkPtr = intersectedSet.find(bbVariables);
+				if(checkPtr == intersectedSet.end())
+				{
+					updated = true;
+				}
 			}
 		}
+
+		bbVariables = intersectedSet;
+		
 		return updated;
 	}
 };
