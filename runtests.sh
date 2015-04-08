51333bd88dd9dd7a6842292c8f4d2bbc79452750
Mark Gray <markmgray2010@gmail.com>
2015-04-08 08:57:13 -0500

    added a script to run all tests in a given directory. Usage: ./runtests.sh ./Output/Loop licm-pass (this will run all tests in the directory passed as the first argument using the licm-pass)

---
+++ runtests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+for i in $1*.c
+do
+	clang -emit-llvm -c $i -o ${i%.c}.bc
+	opt -mem2reg $i.bc -o ${i%.c}.bc
+done
+
+make clean
+
+if [ "$2" == "licm-pass" ]
+then
+	make $2
+	for i in $1*.bc
+	do
+		echo "====================== $i ==================="
+		opt -stats -loop-simplify -mergereturn -instnamer -load ./$2.so -$2 $i -o $i
+		#llvm-dis $i -o ${i%.bc}.ll
+	done
+fi
