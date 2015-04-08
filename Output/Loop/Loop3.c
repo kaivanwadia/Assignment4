0d6f3a0f24f5f393fc55ed9d61172eff8951f099
kaivanwadia <kaivanwadia@gmail.com>
2015-04-08 17:55:55 -0500

    NEw test files for licm

---
+++ Output/Loop/Loop3.c
@@ -0,0 +1,28 @@
+int sum(int a, int b, int c)
+{
+	int c = b*2;
+	int x = a*2;
+	int y = 0;
+	for (int i = 0; i < b; i++)
+	{
+		x = a+b;
+		y = x*c;
+		for (int j = 0; j < c; j++)
+		{
+			y= y*2;
+		}
+	}
+	int ret = a*x;
+	return x;
+}
+
+int main()
+{
+	int x = sum(2, 0, 3);
+	printf("Loop not run : %d\n", x);
+	x = sum(2, 5, 5);
+	printf("Loop run : %d\n", x);
+	x = sum(2, 5, 0);
+	printf("Loop run : %d\n", x);
+	return 0;
+}
